52b1c62e1d9462b635219098401d4fa9a3afd164
Kamal Singh <kamalsingh200238@gmail.com>
2022-09-22 14:26:12 +0530

    Added darkmode

---
+++ components/layout.jsx
@@ -0,0 +1,26 @@
+export default function Layout({ children }) {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (typeof window !== "undefined") {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    // Whenever the user explicitly chooses light mode
+    localStorage.theme = "light";
+
+    // Whenever the user explicitly chooses dark mode
+    localStorage.theme = "dark";
+
+    // Whenever the user explicitly chooses to respect the OS preference
+    localStorage.removeItem("theme");
+  } else {
+    console.log("you are on server");
+  }
+  return <>{children}</>;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>working</div>;
+  return <div className="dark:bg-purple-500 bg-emerald-700">working</div>;
 }
