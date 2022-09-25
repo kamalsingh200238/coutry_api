3bfa08f34e484a502391addaca454fecf9cc127b
Kamal Singh <kamalsingh200238@gmail.com>
2022-09-26 01:16:01 +0530

    Added Search functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
-import getAllCountryData from "../lib/getAllCountryData.js";
+import { useEffect, useState } from "react";
 import Head from "next/head";
+
+import getAllCountryData from "../lib/getAllCountryData.js";
 import CountryCard from "../components/CountryCard.jsx";
 
 export async function getStaticProps() {
@@ -12,13 +14,48 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allCountryData }) {
+  // console.log(allCountryData);
+
+  // state for Search
+  const [q, setQ] = useState("");
+
+  // state for filtered country
+  const [filteredAllCountryData, setFilteredAllCountryData] =
+    useState(allCountryData);
+
+  // parameters in which search text need to matched
+  const [searchParams] = useState(["common", "official"]);
+
+  // Filter Logic
+  useEffect(() => {
+    setFilteredAllCountryData(
+      allCountryData.filter((singleCountryData) => {
+        return searchParams.some((singleParam) => {
+          return singleCountryData.name[singleParam]
+            .toLowerCase()
+            .includes(q.toLowerCase());
+        });
+      })
+    );
+  }, [q]);
   return (
     <>
       <Head>
         <title>Country api</title>
       </Head>
+      <label htmlFor="search-form">
+        <input
+          type="search"
+          name="search-form"
+          id="search-form"
+          placeholder="Search for..."
+          value={q}
+          onChange={(e) => setQ(e.target.value)}
+        />
+        <span className="sr-only">Search countries here</span>
+      </label>
       <div>
-        {allCountryData.map((item, index) => (
+        {filteredAllCountryData.map((item, index) => (
           <CountryCard key={index} singleCountryData={item} />
         ))}
       </div>
